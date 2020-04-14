7404bb97461206b784b176e992510245e5f94bcc
Skoze <1034217469@qq.com>
2020-04-15 00:16:56 +0800

    feat: trace-detail header

--- src/components/Trace/trace-detail.js
+++ src/components/Trace/trace-detail.js
@@ -6,34 +6,40 @@ import styles from './trace-detail.css';
 export default function TraceDetail({ trace }) {
   const [spans, setSpans] = useState([]);
   useEffect(() => {
+    let isFetching = true;
     if (trace) {
       setSpans([]);
-      console.log(trace);
       getSpans(trace.id).then(data => {
-        setSpans(data);
+        if (isFetching) {
+          setSpans(data);
+        }
       });
     }
+    return () => {
+      isFetching = false;
+    };
   }, [trace]);
 
   if (!trace) {
     return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
-  }
-  return (
-    <div className={styles['trace-detail']}>
-      <div className={styles['trace-header']}>
-        <div className={`${styles['trace-name']} ${trace.isError && styles['error']}`}>
-          {trace.endpointNames[0]}
+  } else {
+    return (
+      <div className={styles['trace-detail']}>
+        <div className={styles['trace-header']}>
+          <div className={`${styles['trace-name']} ${trace.isError && styles['error']}`}>
+            {trace.endpointNames[0]}
+          </div>
+          <div>
+            <Tag color="black">起始时间</Tag>
+            <span className={styles['trace-data']}>{trace.start}</span>
+            <Tag color="black">持续时间</Tag>
+            <span className={styles['trace-data']}>{trace.duration}ms</span>
+            <Tag color="black">跨度</Tag>
+            <span className={styles['trace-data']}>{spans.length}</span>
+          </div>
         </div>
-        <div>
-          <Tag color="black">起始时间</Tag>
-          <span className={styles['trace-data']}>{trace.start}</span>
-          <Tag color="black">持续时间</Tag>
-          <span className={styles['trace-data']}>{trace.duration}ms</span>
-          <Tag color="black">跨度</Tag>
-          <span className={styles['trace-data']}>0</span>
-        </div>
+        <div className={styles['trace-content']}></div>
       </div>
-      <div className={styles['trace-content']}>{JSON.stringify(spans)}</div>
-    </div>
-  );
+    );
+  }
 }
